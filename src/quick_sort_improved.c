5e5f61ea32a934a687dc8cc7c763a1c484b66b04
Miki Daijo <itsukanoushiro@gmail.com>
2018-06-30 18:51:56 +0900

    first

--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -9,6 +9,26 @@ int A[N];
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+    int i, l, r, pivot;
+    pivot = A[0];
+    if(n == 0) return;
+    for(i = l = 0, r = n; i < r; i++){
+        if(A[i] < pivot){
+            int z = A[l];
+            A[l] = A[i];
+            A[i] = z;
+            l++;
+        }
+        else if(A[i] > pivot){
+            r--;
+            int z = A[r];
+            A[r] = A[i];
+            A[i] = z;
+            i--;
+        }
+    }
+    quick_sort(A, l);
+    quick_sort(A+r, n-r);
 }
 
 int main(){
@@ -19,10 +39,14 @@ int main(){
     A[i] = (long long int) A[i-1] * A[1] % N;
   }
 
-// すべての要素が同じ場合でも計算が早く終わるか確認する
-
   quick_sort(A, N);
   for(i=0;i<N;i++){
     if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+  }
+  
+// すべての要素が同じ場合でも計算が早く終わるか確認する
+  for(i = 0; i < N; i++) A[i] = 0;
+  for(i = 0; i < N; i++){
+    if(A[i] != 0) printf("ERROR %dth element is %d\n", i, A[i]);
   }
 }
